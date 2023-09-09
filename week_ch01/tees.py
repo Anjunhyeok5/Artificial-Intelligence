2097c2708d9d59d67673ca81f71f3e011f472b72
Liam <107667828+Anjunhyeok5@users.noreply.github.com>
2023-09-09 11:19:17 +0900

    week03 v7

--- week_ch01/tees.py
+++ week_ch01/tees.py
@@ -1,9 +1,19 @@
-student_id = 1000
-student_name = "Liam"
-subject = ["python", "stats", "mathmatics"]
-score = 3.4
-teacher = ("lee", "park")
+age = int(input("나이는 : "))
+print(age >= 19)
 
-print(f"학번이 {student_id}인 {student_id}은 {subject}과목을 수강하였고, 학점이 {score}이다.")
-print(type(student_id), type(student_name), type(subject), type(score), type(True), type(False), type(teacher))
-print(id(student_id), id(student_name), id(subject), id(score))
+if age >= 19:
+    print("pass")
+else:
+    print("unpass")
+
+
+
+# student_id = 1000
+# student_name = "Liam"
+# subject = ["python", "stats", "mathmatics"]
+# score = 3.4
+# teacher = ("lee", "park")
+#
+# print(f"학번이 {student_id}인 {student_id}은 {subject}과목을 수강하였고, 학점이 {score}이다.")
+# print(type(student_id), type(student_name), type(subject), type(score), type(True), type(False), type(teacher))
+# print(id(student_id), id(student_name), id(subject), id(score))
