7061c876b574bce60f25c94c51b55f27d5ae0ee5
Liam <107667828+Anjunhyeok5@users.noreply.github.com>
2023-09-16 10:24:02 +0900

    week04 v4

--- week_ch01/week04.py
+++ week_ch01/week04.py
@@ -5,3 +5,10 @@ b = 0b1010110001110001
 c = 0o126161
 d = 44145
 print(a, b, c, b)
+print(hex(d), bin(d), oct(c), d)
+print(chr(90)) # A ~ Z (65 ~ 90)
+print(ord('B'))
+# a ~ z (97 ~ 122)
+print(chr(97))
+print(chr(44608))
+print(hex(ord("김")))
