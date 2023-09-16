756370d8aac40fd619bd62377296a5daba32d129
Liam <107667828+Anjunhyeok5@users.noreply.github.com>
2023-09-16 11:36:04 +0900

    week04 v7

---
+++ week_ch01/week04_if.py
@@ -0,0 +1,14 @@
+vowels = ['a', 'e', 'i', 'o', 'u']
+letter = input("input alphabet letter : ")
+if letter in vowels:
+    print(f"{letter} is a vowel")
+else:
+    print(f"{letter} is not a vowel")
+
+
+# vowels = "aeiou"
+# letter = input("input alphabet letter : ")
+# if letter in vowels:
+#     print(f"{letter} is a vowel")
+# else:
+#     print(f"{letter} is not a vowel")
