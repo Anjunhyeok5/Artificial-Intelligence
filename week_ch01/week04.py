47cd1570d6e3b6db5df3a7380adf8410a7640ed0
Liam <107667828+Anjunhyeok5@users.noreply.github.com>
2023-09-16 11:28:23 +0900

    week04 v6

--- week_ch01/week04.py
+++ week_ch01/week04.py
@@ -1,13 +1,23 @@
-pi = 3.141592
-pi_e3 = 0.003141592e3
-print(pi, pi_e3)
-print(bool(-19))
-print(bool(1))
-print(bool(1.0e2))
+print(bool(None))
+print(bool(""))
+print(bool[''])
+print(bool([]))
+print(bool(()))
+print(bool({}))
+print(bool(set()))
 print(bool(0))
-print(bool(0.0))
-print(int('F', 16))
-print(float("99.1"))
+
+
+# pi = 3.141592
+# pi_e3 = 0.003141592e3
+# print(pi, pi_e3)
+# print(bool(-19))
+# print(bool(1))
+# print(bool(1.0e2))
+# print(bool(0))
+# print(bool(0.0))
+# print(int('F', 16))
+# print(float("99.1"))
 # print(int("10e5"))
-print(float("10e5"))
-print(True + 2.3)
+# print(float("10e5"))
+# print(True + 2.3)
