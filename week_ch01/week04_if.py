d1e210f7127bb6e9ed738483bdcfdccbf2066f03
Liam <107667828+Anjunhyeok5@users.noreply.github.com>
2023-09-16 11:39:48 +0900

    week04 v8

--- week_ch01/week04_if.py
+++ week_ch01/week04_if.py
@@ -1,9 +1,18 @@
-vowels = ['a', 'e', 'i', 'o', 'u']
-letter = input("input alphabet letter : ")
-if letter in vowels:
-    print(f"{letter} is a vowel")
-else:
-    print(f"{letter} is not a vowel")
+army = """
+우리는 국가와 국민에 충성을 다하는 대한민국 육군이다.
+하나, 우리는 자유 민주주의 를 수호하며 조국 통일의 역군이 된다.
+둘, 우리는 실전과 같은 훈련으로 지상전의 승리자가 된다.
+"""
+
+print(army)
+
+
+# vowels = ['a', 'e', 'i', 'o', 'u']
+# letter = input("input alphabet letter : ")
+# if letter in vowels:
+#     print(f"{letter} is a vowel")
+# else:
+#     print(f"{letter} is not a vowel")
 
 
 # vowels = "aeiou"
