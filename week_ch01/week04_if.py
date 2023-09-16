9945082f7a313d95e6fd24c60f407700cef97df7
Liam <107667828+Anjunhyeok5@users.noreply.github.com>
2023-09-16 11:50:05 +0900

    week04 v10

--- week_ch01/week04_if.py
+++ week_ch01/week04_if.py
@@ -1,12 +1,17 @@
-army = """
-우리는 국가와 국민에 충성을 다하는 대한민국 육군이다.
-하나, 우리는 자유 민주주의 를 수호하며 조국 통일의 역군이 된다.
-둘, 우리는 실전과 같은 훈련으로 지상전의 승리자가 된다.
-"""
+a = input()
+b = input()
+print(a + b)
 
-high_school = "Incheon \"Science\" high school"
-print(high_school)
-print(army)
+
+# army = """
+# 우리는 국가와 국민에 충성을 다하는 대한민국 육군이다.
+# 하나, 우리는 자유 민주주의 를 수호하며 조국 통일의 역군이 된다.
+# 둘, 우리는 실전과 같은 훈련으로 지상전의 승리자가 된다.
+# """
+#
+# high_school = "Incheon \"Science\" high school"
+# print(high_school)
+# print(army)
 
 
 # vowels = ['a', 'e', 'i', 'o', 'u']
